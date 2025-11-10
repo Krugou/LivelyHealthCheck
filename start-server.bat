6861685cc08c661750ed057647fb4754898e8bdd
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-11-10 11:20:34 +0200

    Create start-server.bat

---
+++ start-server.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Start the LivelyHealthCheck server in production mode
+
+REM Start the server with production environment in a new window
+start "LivelyHealthCheck Server" cmd /k npm run start
+
